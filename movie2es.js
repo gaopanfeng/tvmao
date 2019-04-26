b044db883a8961922a9ea9053dc38006fdc66d8c
gaopanfeng <gaopanfeng@sogou-inc.com>
2019-04-26 19:31:02 +0800

    movie2es

--- movie2es.js
+++ movie2es.js
@@ -1,6 +1,4 @@
-const fs = require('fs');
 const elasticsearch = require('elasticsearch');
-const nunjucks = require('nunjucks');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const getUa = require('./getUa');
@@ -9,19 +7,19 @@ const client = new elasticsearch.Client({
     log: 'warning'
 });
 
-async function getMovies(){
+async function getMovies() {
     let ret = await client.search({
         index: 'tvmao-movie',
         type: 'movie',
         body: {
-            size: 1000
+            size: 10000
         }
     });
 
     let movies = new Map();
     ret.hits.hits.map(it=>it._source).forEach(it=>{
-        movies.set(it.href,it.content);
-    })
+        movies.set(it.href, it.content);
+    });
     return movies;
 }
 
@@ -36,7 +34,7 @@ async function getMovies(){
                 movie: {
                     terms: {
                         field: 'name.keyword',
-                        size: 1000
+                        size: 10000
                     },
                     aggs: {
                         list: {
@@ -62,45 +60,33 @@ async function getMovies(){
         }
     });
 
-    // let movies_ret = await client.search({
-    //     index: 'tvmao-movie',
-    //     type: 'movie',
-    //     body: {
-    //         size: 1000
-    //     }
-    // });
-
-    //let movies = movies_ret.hits.hits.map(it=>it._source);
-
-    // console.log(JSON.stringify(ret,null,4));
-
     let movies = ret.aggregations.movie.buckets.map(it=>it.list.hits.hits[0]._source);
     movies = movies.filter(it=>!movieMap.has(it.href));
-    for(let movie of movies){
+    for (let movie of movies) {
         let href = movie.href;
         let name = movie.name;
-        let id = href.substring(href.lastIndexOf('/')+1);
-        let movieret = await axios.get(href.replace('https://','http://'),{
-            headers:{
-                'User-Agent':getUa()
+        let id = href.substring(href.lastIndexOf('/') + 1);
+        let movieret = await axios.get(href.replace('https://', 'http://'), {
+            headers: {
+                'User-Agent': getUa()
             }
         });
-        console.log(href,movieret.status);
-        if(movieret.status === 200){
+        console.log(href, movieret.status);
+        if (movieret.status === 200) {
             let content = movieret.data;
             const $ = cheerio.load(content);
             const text = $('.obj_meta').text();
             await client.index({
-                index:'tvmao-movie',
-                type:'movie',
-                id:id,
-                body:{
-                    name:name,
-                    href:href,
-                    content:text
+                index: 'tvmao-movie',
+                type: 'movie',
+                id: id,
+                body: {
+                    name: name,
+                    href: href,
+                    content: text
                 }
             });
         }
-    }    
+    }
 })();
 
